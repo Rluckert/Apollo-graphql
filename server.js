c470557b5be4cebb1ac54096a7cca2756c755b43
Rluckert <rafael.luckert@hotmail.com>
2019-07-19 11:14:16 -0500

    Crud end

--- server.js
+++ server.js
@@ -10,21 +10,76 @@ type Course{
     views: Int
 }
 
+type Alert{
+    message: String
+}
+
+input CourseInput{
+    title: String!
+    views: Int
+}
+
 type Query{
     getCourses(page: Int, limit: Int = 1): [Course]
     getCourse(id: ID!): Course
 }
+
+type Mutation{
+    addCourse(input: CourseInput): Course
+    updateCourse(id: ID!, input: CourseInput): Course
+    deleteCourse(id: ID!): Alert
+}
 `;
+
+const resolvers = {
+    Query: {
+        getCourses(obj, { page, limit }) {
+            if (page !== undefined) {
+                return courses.slice(page * limit, (page + 1) * limit);
+            }
+            return courses;
+        },
+        getCourse(obj, { id }){
+            return courses.find( (course) => id == course.id);
+         }
+    },
+    Mutation: {
+        addCourse(obj, { input }) {
+            const { title, views } = input;
+            const id = String(courses.length + 1);
+            const course = { id, title, views };
+            courses.push(course);
+            return course;
+        },
+        updateCourse(obj, {id, input}){
+            const { title, views } = input;
+            const courseIndex = courses.findIndex((course) => id == course.id);
+            const course = courses[courseIndex];
+    
+            const newCourse = Object.assign(course, { title, views });
+            courses[courseIndex] = newCourse;
+    
+            return newCourse;
+        },
+        deleteCourse(obj, {id}){
+            courses = courses.filter(course => course.id != id );
+            return {
+                message: `El curso con id: ${id} ha sido eliminado`
+            }
+        }
+
+    }
+}
 
 const schema = makeExecutableSchema({
     typeDefs,
-    resolvers: {}
+    resolvers
 })
 
 const server = new ApolloServer({
     schema
 });
 
-server.listen().then(({url}) => {
+server.listen().then(({ url }) => {
     console.log(`Servidor iniciado en ${url}`);
 })
